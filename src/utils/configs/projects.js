74af01917b15ab52aa3c64be566415efa90b58ed
WARDI-Kamal <kha0s@free.fr>
2022-06-15 04:50:31 +0400

    wip : global structure built

---
+++ src/utils/configs/projects.js
@@ -0,0 +1,47 @@
+export const project_details = [
+  {
+    "id": "",
+    "created_at": "",
+    "title": "",
+    "description": "",
+    "thumbnail_path": "",
+    "started_date": "",
+    "ended_date": "",
+    "front_stack": "",
+    "back_stack": "",
+    "type": "",
+    "status": "",
+    "purpose": "",
+    "is_published": "",
+    "repository_path": "",
+    "demo_path": "",
+    "dev-log_path": "",
+  }
+];
+
+export const testimonials = [
+  {
+    "id": "",
+    "name": "",
+    "content": "",
+    "is_validate": "",
+  }
+];
+
+export const dev_logs = [
+  {
+    "id": "",
+    "title": "",
+    "content": "",
+    "is_published": ""
+  }
+]
+
+export const ext_ressources = [
+  {
+    "devlog_id": "",
+    "name": "",
+    "description": "",
+    "url": ""
+  }
+]
---
+++ src/utils/services/api.js
@@ -0,0 +1,38 @@
+export {}
+
+/**
+ * SUPABASE TABLES
+ * 
+ * dev_logs : articles that will be written during development.
+ * ext_ressources : links of ressources that will be used in to write articles.
+ * images : images that will be used to present myself, projects and articles.
+ * projects : list of projects that will be presented in the website.
+ * testimonials : list of testimonials that will be written about my work or collaborations.
+ * traductions : list of traductions of all contents in the website.
+ * urls : list of urls of projects or articles.
+ */
+
+/**
+ * get all projects from supabase table
+ */
+
+/**
+ * get all urls for a given project from supabase table
+ */
+
+/**
+ * get dev_logs from supabase table for a given project
+ */
+
+/**
+ * get images from supabase table for a given project
+ */
+
+/**
+ * get all testimonials for a given project from supabase table
+ */
+/**
+ * get localizations of descriptions/name/alt/purpose from supabase table for a given project
+ */
+
+// build a json file for each project with all the data needed to build the website
