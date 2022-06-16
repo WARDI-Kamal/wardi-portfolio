417665ff8777d955cc27872379d375d24af92382
WARDI-Kamal <kha0s@free.fr>
2022-06-17 00:32:33 +0400

    wip : full reset to switch on React project

--- src/utils/services/api.test.js
+++
@@ -1,7 +0,0 @@
-// test if numbers are correctly parsed if they are passed as strings
-
-// test if data are correctly pulled from the database
-
-// test if the data is correctly displayed in the page
-
-// test if data are correctly pushed to the database (if i had implemented create logic in the backend)
